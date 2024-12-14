aafe40d210879d8be827ba2510bc318ca272acb0
Ron Jeffries <ronjeffries@acm.org>
2024-12-14 08:57:40 -0500

    hypotenuse

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 stack = []
@@ -15,11 +17,13 @@ def number(n):
     stack.append(n)
 
 def interpret(s):
+    print(f'interpret {s}')
     words = s.split()
     for word in words:
         interpret_word(word)
 
 def interpret_word(word):
+    print(f'word {word}')
     if is_number(word):
         stack.append(int(word))
     elif word == '+':
@@ -28,15 +32,28 @@ def interpret_word(word):
         # ( x1 x2 == x1 - x2
         x2, x1 = stack.pop(), stack.pop()
         stack.append(x1 - x2)
-def interpret_word(word):
-    if is_number(word):
-        stack.append(int(word))
-    elif word == '+':
-        plus()
-    elif word == '-':
-        # ( x1 x2 == x1 - x2
+    elif word == '*':
         x2, x1 = stack.pop(), stack.pop()
-        stack.append(x1 - x2)
+        stack.append(x1 * x2)
+    elif word == 'SQRT':
+        stack.append(math.sqrt(stack.pop()))
+    elif word == 'SWAP':
+        # ( x1 x2 -- x2 x1 )
+        x2, x1 = stack.pop(), stack.pop()
+        stack.append(x2); stack.append(x1)
+    elif word == 'DUP':
+        x1 = stack.pop()
+        stack.append(x1); stack.append(x1)
+    elif word == 'NEGATE':
+        s = '0 SWAP -'
+        interpret(s)
+    elif word == 'SQUARE':
+        interpret('DUP *')
+    elif word == 'HYPOTENUSE':
+        s = 'SQUARE SWAP SQUARE + SQRT'
+        interpret(s)
+    else:
+        raise ValueError(f'Invalid word {word}')
 
 def is_number(s):
     try:
@@ -71,3 +88,48 @@ class TestInitial:
         s = '100 49 -'
         interpret(s)
         assert stack.pop() == 51
+
+    def test_swap(self):
+        clear()
+        s = '1 2 SWAP'
+        interpret(s)
+        assert stack.pop() == 1
+        assert stack.pop() == 2
+
+    def test_dup(self):
+        clear()
+        s = '100 DUP'
+        interpret(s)
+        assert stack.pop() == 100
+        assert stack.pop() == 100
+
+    def test_times(self):
+        clear()
+        s = '5 6 *'
+        interpret(s)
+        assert stack.pop() == 30
+
+    def test_negate(self):
+        clear()
+        s = '100 NEGATE'
+        interpret(s)
+        assert stack.pop() == -100
+
+    def test_square(self):
+        clear()
+        s = '10 SQUARE'
+        interpret(s)
+        assert stack.pop() == 100
+
+    def test_sqrt(self):
+        clear()
+        s = '4 SQRT'
+        interpret(s)
+        assert stack.pop() == 2
+
+    def test_hyp(self):
+        clear()
+        s = '3 4 HYPOTENUSE'
+        interpret(s)
+        assert stack.pop() == 5
+        assert False
