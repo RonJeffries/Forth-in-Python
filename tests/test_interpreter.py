74145428863daf717fce0eba740e95c619d4ab37
Ron Jeffries <ronjeffries@acm.org>
2025-01-31 08:31:44 -0500

    increment

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -2,9 +2,14 @@
 
 
 class IForth:
-    pass
+    def __init__(self):
+        self.stack = []
 
 
 class TestInterpreter:
     def test_exists(self):
         forth = IForth()
+
+    def test_stack(self):
+        forth = IForth()
+        assert forth.stack == []
