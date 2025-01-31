efcfb80868fe4da0f14c81f7777d4e8fc60aaed0
Ron Jeffries <ronjeffries@acm.org>
2025-01-31 08:57:55 -0500

    increment

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -5,13 +5,25 @@ class IForth:
     def __init__(self):
         self.stack = []
 
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        return self.stack.pop()
+
     @staticmethod
     def make_tokens(text):
         return text.split()
 
+    def execute_line(self, line):
+        for token in self.make_tokens(line):
+            self.execute(token)
+
     def execute(self, command):
         if command == 'dup':
             self.stack.append(self.stack[-1])
+        elif command == "+":
+            self.push(self.pop() + self.pop())
 
 
 class TestInterpreter:
@@ -32,3 +44,16 @@ class TestInterpreter:
         forth.stack.append(10)
         forth.execute('dup')
         assert forth.stack == [10, 10]
+
+    def test_plus(self):
+        forth = IForth()
+        forth.stack.append(10)
+        forth.stack.append(32)
+        forth.execute('+')
+        assert forth.stack == [42]
+
+    def test_dup_plus(self):
+        forth = IForth()
+        forth.stack.append(21)
+        forth.execute_line('dup +')
+        assert forth.stack == [42]
