7ae2a765e70e3208c3cdf5ecc23b242625d3bc39
Ron Jeffries <ronjeffries@acm.org>
2025-02-26 09:12:22 -0500

    move StringProvider to source side.

---
+++ source/string_provider.py
@@ -0,0 +1,15 @@
+class StringProvider:
+    def __init__(self, text):
+        self.input_line = text
+
+    def has_tokens(self):
+        return self.input_line
+
+    def next_token(self):
+        trimmed = self.input_line.strip()
+        index = trimmed.find(' ')
+        if index == -1:
+            token, self.input_line = trimmed.upper(), ''
+        else:
+            token, self.input_line = trimmed[:index].upper(), trimmed[index+1:].strip()
+        return token
