2827bb9af31d79bc8c5d41b5bf5c03d5e1d77577
Ron Jeffries <ronjeffries@acm.org>
2025-03-03 10:29:42 -0500

    rudimentary prompt with backspace.

--- game_main.py
+++ game_main.py
@@ -14,15 +14,8 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption('ForthBots')
-
-    def main_loop(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            self.screen.fill("midnightblue")
-            lines = [
+        self.lines = [
+                'Forth> _',
                 '4 ok',
                 'Forth> 2 2 +',
                 'alpha', 'bravo', 'charlie', 'delta', 'echo',
@@ -32,9 +25,25 @@ class Game:
                 'tango', 'uniform', 'victor', 'whiskey', 'xray',
                 'yankee', 'zulu'
             ]
+
+    def main_loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        self.lines.insert(0, 'Forth> _')
+                    elif event.key == pygame.K_BACKSPACE:
+                        self.lines[0] = self.lines[0][:-2] + '_'
+                    else:
+                        self.lines[0] = self.lines[0][:-1] + event.unicode + '_'
+            self.screen.fill("midnightblue")
+
             rect = self.screen.get_rect()
-            y = rect.bottomleft[1] - 20
-            for line in lines:
+            y = rect.bottomleft[1] - 24
+            for line in self.lines:
                 self.text((10,y),line, 16, self.WHITE, "midnightblue")
                 y -= 20
             pygame.display.flip()
