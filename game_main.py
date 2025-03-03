821269441229b15ccc2be57d5dd9108930608d82
Ron Jeffries <ronjeffries@acm.org>
2025-03-03 08:48:24 -0500

    hello world

--- game_main.py
+++ game_main.py
@@ -1,7 +1,15 @@
 
 import pygame
 
+
+
 class Game:
+    WHITE = (232, 232, 232)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLACK = (0, 0, 0)
+    BASE_FONT = 'Courier New.ttf'
+
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
@@ -14,7 +22,16 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
             self.screen.fill("midnightblue")
+            self.text((100,100), "Hello, World!", 16, self.WHITE, self.BLACK)
             pygame.display.flip()
+
+    def text(self, location, phrase, size, front_color, back_color):
+        font = pygame.font.Font(self.BASE_FONT, size)
+        font.set_bold(True)
+        text = font.render(phrase, True, front_color, back_color)
+        text_rect = text.get_rect()
+        text_rect.topleft = location
+        self.screen.blit(text, text_rect)
 
 if __name__ == '__main__':
     game = Game()
