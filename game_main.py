c06a375dbe7d28ffbbab66946db47c2ff0606108
Ron Jeffries <ronjeffries@acm.org>
2025-03-05 09:14:16 -0500

    Right pane of Game now calls Forth to get results.

--- game_main.py
+++ game_main.py
@@ -1,6 +1,9 @@
+import sys
+from io import StringIO
 
 import pygame
 
+from source.forth import Forth
 
 
 class Game:
@@ -18,33 +21,28 @@ class Game:
 
     def __init__(self):
         pygame.init()
+        self.forth = Forth()
+        self.running = False
         self.screen = pygame.display.set_mode(self.WINDOW_SIZE)
         pygame.display.set_caption('ForthBots')
         self.lines = [
-                'Forth> _',
-                '4 ok',
-                'Forth> 2 2 +',
-                'alpha', 'bravo', 'charlie', 'delta', 'echo',
-                'foxtrot', 'golf', 'hotel', 'india', 'juliett',
-                'kilo', 'lima', 'mike', 'november',
-                'oscar', 'papa', 'quebec', 'romeo', 'sierra',
-                'tango', 'uniform', 'victor', 'whiskey', 'xray',
-                'yankee', 'zulu'
+                'Forth> _'
             ]
 
+    def call_forth(self, line):
+        original_stdout = sys.stdout
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        try:
+            result = self.forth.compile(line)
+        finally:
+            sys.stdout = original_stdout
+        return captured_output.getvalue() + result
+
     def main_loop(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        self.lines.insert(0, 'Forth> _')
-                    elif event.key == pygame.K_BACKSPACE:
-                        self.lines[0] = self.lines[0][:-2] + '_'
-                    else:
-                        self.lines[0] = self.lines[0][:-1] + event.unicode + '_'
+        self.running = True
+        while self.running:
+            self.process_events()
             self.screen.fill("midnightblue")
             pygame.draw.line(self.screen, self.WHITE,
                              (self.BORDER, 0),
@@ -56,6 +54,28 @@ class Game:
                 self.text((x,y),line, 16, self.WHITE, "midnightblue")
                 y -= 20
             pygame.display.flip()
+
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.process_keystroke(event)
+
+    def process_keystroke(self, event):
+        if event.key == pygame.K_RETURN:
+            self.append_forth_result()
+            self.lines.insert(0, 'Forth> _')
+        elif event.key == pygame.K_BACKSPACE:
+            self.lines[0] = self.lines[0][:-2] + '_'
+        else:
+            self.lines[0] = self.lines[0][:-1] + event.unicode + '_'
+
+    def append_forth_result(self):
+        self.lines[0] = self.lines[0][:-1]
+        forth_line = self.lines[0][6:]
+        result = self.call_forth(forth_line)
+        self.lines.insert(0, result)
 
     def text(self, location, phrase, size, front_color, back_color):
         font = pygame.font.Font(self.BASE_FONT, size)
